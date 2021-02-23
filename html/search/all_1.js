29037be2c7647740302989a0737cbacb890d6c33
zejun <li.zeju@husky.neu.edu>
2021-02-23 00:48:08 -0500

    updated documentation

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['ball_1',['Ball',['../class_ball.html',1,'Ball'],['../class_ball.html#a86a144d3dad6c953e422e32435923bbb',1,'Ball::Ball()'],['../class_ball.html#ae042cfcdf94864c4d06d3c8650de7dfe',1,'Ball::Ball(Vec pos, Vec v)']]],
-  ['board_2',['board',['../class_brick_board.html#ad601c2d214734fbb5f19743fc35afc4a',1,'BrickBoard']]],
+  ['board_2',['board',['../class_brick_board.html#a28aff289173d4166c3181bd9c52addd3',1,'BrickBoard']]],
   ['breakout_3',['Breakout',['../index.html',1,'']]],
   ['brick_4',['Brick',['../class_brick.html',1,'Brick'],['../class_brick.html#aa29d678c3d901c7b18cbc3a27a51be17',1,'Brick::Brick()'],['../class_brick.html#a67d8c8b248fc101690f5b66b6265df69',1,'Brick::Brick(Uint8 score, Vec pos)']]],
   ['brickboard_5',['BrickBoard',['../class_brick_board.html',1,'BrickBoard'],['../class_brick_board.html#a51338202b156746437eb1025d5885857',1,'BrickBoard::BrickBoard()']]],
--- html/search/variables_0.js
+++ html/search/variables_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['board_125',['board',['../class_brick_board.html#ad601c2d214734fbb5f19743fc35afc4a',1,'BrickBoard']]],
+  ['board_125',['board',['../class_brick_board.html#a28aff289173d4166c3181bd9c52addd3',1,'BrickBoard']]],
   ['brickscore_126',['brickScore',['../class_brick.html#a23bbb1506cbbcd8acd1ae7b4ad5a2c6d',1,'Brick']]]
 ];
